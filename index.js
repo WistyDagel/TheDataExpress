bd260aa11c2329a3e343ceb8de95363c8ca24e3f
WistyDagel <chrisdazley@gmail.com>
2019-11-12 08:57:12 -0700

    All the set up was made
    Mongo integration still needed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expressSession = require('express-session');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const path = require('path');
+const bcrypt = require('bcrypt-nodejs');
+const routes = require('routes/routes.js');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname + '/views'));
+app.use(express.static(path.join(__dirname + '/public')));
+
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+app.listen(3000);
